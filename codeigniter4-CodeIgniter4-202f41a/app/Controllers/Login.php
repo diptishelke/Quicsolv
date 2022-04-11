05da1d7ec9fb77fb4817693a0ee641fbe25a5943
unknown <diptishelke8514@gmail.com>
2022-04-11 17:03:24 +0530

    project

--- codeigniter4-CodeIgniter4-202f41a/app/Controllers/Login.php
+++ codeigniter4-CodeIgniter4-202f41a/app/Controllers/Login.php
@@ -3,9 +3,8 @@
 namespace App\Controllers;
 
 use App\Models\Usermodel;
-
+use CodeIgniter\Files\File;
 use App\Controllers\BaseController;
-use  hash;
 
 class Login extends BaseController
 {
@@ -16,7 +15,7 @@ class Login extends BaseController
         return view('login-form', ['errors' => []]);
     }
     public function submit()
-    { 
+    {
         $validationRule = [
             "email" => "required",
             "password" => "required"
@@ -25,7 +24,7 @@ class Login extends BaseController
         $data['table'] = $usrmodel->findAll();
         $result = $usrmodel->where('email', $this->request->getvar('email'))->first();
         $password = $usrmodel->pswverify($this->request->getvar('password'), $result['password']);
-         // print_r($password);exit;
+        // print_r($password);exit;
 
         $session = session();
         if ($password) {
@@ -55,29 +54,20 @@ class Login extends BaseController
 
     {
         $usrmodel = new Usermodel();
-        $id = $this->request->getvar('id');
-        $config['upload_path']          = './assets/images/';
-        $config['allowed_types']        = 'gif|jpg|png';
-        $config['max_size']             = 100;
-        $config['max_width']            = 1024;
-        $config['max_height']           = 768;
-
-        $this->load->library('upload', $config);
-       
-        if (!$this->upload->do_upload('image')) {
-            echo $this->upload->display_errors();
-        } else {
-            $fd = $this->upload->data();
-            $fn = $fd['file_name'];
-            $data = [
-                'name' => $this->request->getvar('name'),
-                'last-name' => $this->request->getvar('last-name'),
-                'phone' => $this->request->getvar('phone'),
-                'image' => $fn
-                //'email' => $this->request->getvar('email'),
-            ];
+        $file = $this->request->getFile('image');
+        if ($file->isValid() && !$file->hasMoved()) {
+            $imagename = $file->getRandomName();
+            $file->move('uploads/', $imagename);
         }
-
+        $id = $this->request->getvar('id');
+        $data = [
+            'name' => $this->request->getvar('name'),
+            'last-name' => $this->request->getvar('last-name'),
+            'phone' => $this->request->getvar('phone'),
+            'image' => $imagename,
+            //'email' => $this->request->getvar('email'),
+        ];
+        //print_r($data);exit;
 
         if ($this->request->getpost('password') != '') {
             $data['password'] = $this->request->getvar('password');
